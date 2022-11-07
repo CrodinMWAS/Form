1dcd75619c9e4043f341f310f47268dd3da8dc1a
CrodinMWAS <71005489+CrodinMWAS@users.noreply.github.com>
2022-11-07 10:42:27 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+function elkuld() {
+    document.getElementById("p1").innerHTML=inp1.value;
+    document.getElementById("p2").innerHTML=inp2.value;
+    document.getElementById("p3").innerHTML=inp3.value;
+}
+
+function klikk(par) {
+    par.style.background="lightblue";
+}
+
+function off(par) {
+    par.style.background="white";
+}
